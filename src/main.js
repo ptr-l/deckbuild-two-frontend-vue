a506fe3f5e5a1bbb18d376bc46cca1158cd8e27e
Peter Lamson <ptrlmsn@gmail.com>
2022-03-24 17:40:04 -0400

    vuestic setup

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { VuesticPlugin } from 'vuestic-ui';
 import App from './App.vue';
 import router from './router';
+import 'vuestic-ui/dist/vuestic-ui.css';
 
 const app = createApp(App);
 
 app.use(createPinia());
+app.use(VuesticPlugin);
 app.use(router);
 app.mount('#app');
